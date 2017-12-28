486b97e2f419f72b2bc9a4bcc414d1fa2f036f4f
Seongwoo Hong <seongwoo.hong@duke.edu>
2017-12-27 19:20:05 -0500

    사이트 첫 재개설
    시험용으로 첫 업로드 시도합니다.

---
+++ assets/js/lunr-en.js
@@ -0,0 +1,81 @@
+var idx = lunr(function () {
+  this.field('title', {boost: 10})
+  this.field('excerpt')
+  this.field('categories')
+  this.field('tags')
+  this.ref('id')
+});
+
+
+
+  
+  
+    idx.add({
+      title: "Reopened Blog",
+      excerpt: "I finally had a chance to reopen my github blog. I would utilize this blog for continuous log of myself...",
+      categories: ["Daily Life"],
+      tags: ["Jekyll","Blog","Daily Life"],
+      id: 0
+    });
+    
+  
+
+
+console.log( jQuery.type(idx) );
+
+var store = [
+  
+    
+    
+    
+      
+      {
+        "title": "Reopened Blog",
+        "url": "/daily%20life/2017/12/27/Reopened-Blog/",
+        "excerpt": "I finally had a chance to reopen my github blog. I would utilize this blog for continuous log of myself...",
+        "teaser":
+          
+            null
+          
+      }
+    
+  ]
+
+$(document).ready(function() {
+  $('input#search').on('keyup', function () {
+    var resultdiv = $('#results');
+    var query = $(this).val();
+    var result = idx.search(query);
+    resultdiv.empty();
+    resultdiv.prepend('<p class="results__found">'+result.length+' Result(s) found</p>');
+    for (var item in result) {
+      var ref = result[item].ref;
+      if(store[ref].teaser){
+        var searchitem =
+          '<div class="list__item">'+
+            '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
+              '<h2 class="archive__item-title" itemprop="headline">'+
+                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
+              '</h2>'+
+              '<div class="archive__item-teaser">'+
+                '<img src="'+store[ref].teaser+'" alt="">'+
+              '</div>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt+'</p>'+
+            '</article>'+
+          '</div>';
+      }
+      else{
+    	  var searchitem =
+          '<div class="list__item">'+
+            '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
+              '<h2 class="archive__item-title" itemprop="headline">'+
+                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
+              '</h2>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt+'</p>'+
+            '</article>'+
+          '</div>';
+      }
+      resultdiv.append(searchitem);
+    }
+  });
+});
